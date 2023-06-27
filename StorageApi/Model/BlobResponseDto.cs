987f98eb30e857a024a5490ff328571db3600f7a
Manas K <Manas_1684841872928@npmindtreelabs.onmicrosoft.com>
2023-06-27 11:36:05 +0000

    Added 2 files to /StorageApi/Model

---
+++ StorageApi/Model/BlobResponseDto.cs
@@ -0,0 +1,12 @@
+
+public class BlobResponseDto
+    {
+        public string? Status { get; set; }
+        public bool Error { get; set; }
+        public BlobDto Blob { get; set; }
+
+        public BlobResponseDto()
+        {
+            Blob = new BlobDto();
+        }
+    }
