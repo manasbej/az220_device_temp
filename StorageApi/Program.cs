300f407ed2d54a579223bd8305446875c0fa211d
Manas K <Manas_1684841872928@npmindtreelabs.onmicrosoft.com>
2023-06-27 11:38:26 +0000

    Updated Program.cs

--- StorageApi/Program.cs
+++ StorageApi/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IAzureFileShareService, AzureFileShareService>();
+builder.Services.AddScoped<ITableStorageService, TableStorageService>();
+builder.Services.AddScoped<IAzureQueue, AzureQueue>();
+builder.Services.AddScoped<IDeviceManagerService, DeviceManagerService>();
+builder.Services.AddScoped<IAzureBlobService, AzureBlobService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
